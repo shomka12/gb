368d972841ca53ed5400d17669120b516f7278a6
shomka12 <shaman_p@mail.ru>
2021-10-01 15:53:18 +0500

    no message

--- prof_css/Js1/1/script_2.js
+++ prof_css/Js1/1/script_2.js
@@ -1,4 +1,8 @@
-var admin, name;
-name="Василий";
-admin=name;
-alert(admin);
+var a=parseInt(prompt('Введите A: '));
+var b=parseInt(prompt('Введите B: '));
+a = a + b; 
+b = a - b; 
+a = a - b;
+
+alert('Теперь A = ' + a + ', B = ' + b)
+
--- prof_css/Js1/1/script_3.js
+++ prof_css/Js1/1/script_3.js
@@ -1,1 +1,6 @@
+var admin, name = prompt('Введите Имя');
+admin=name;
+alert(admin);
+alert('Теперь A = ' + a + ', B = ' + b)
+
 alert(1000 + "108")
