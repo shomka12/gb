2a69547473f8a5d9fde1f114bc03a890355c5aeb
shomka12 <shaman_p@mail.ru>
2021-10-04 22:56:10 +0500

    no message

---
+++ test_js/js1.js
@@ -0,0 +1,6 @@
+var question1 = prompt('Введите обязательную статью расходов в этом месяце'),
+    question2 = prompt('Во сколько обойдется?'),
+    
+    expenses = {
+        question1: question2
+    };
