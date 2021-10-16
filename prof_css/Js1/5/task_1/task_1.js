8b2e34759de819675f1692f8b88308af62f3b140
shomka <shaman_p@mail.ru>
2021-10-16 17:30:50 +0500

    шахматы

--- prof_css/Js1/5/task_1/task_1.js
+++ prof_css/Js1/5/task_1/task_1.js
@@ -1,14 +1,23 @@
-var table = document.createElement('table');
-for (i = 1; i <= 8; i++) {
-    var tr = document.createElement('tr');
-    table.append(tr);
-    // tr.classList.add('cell');
-    for (j = 1; j <= 8; j++) {
-        var td = document.createElement('td');
-        td.classList.add('cell');
-        td.innerHTML = "<h2>" + (i * j) + "&#9816;</h2";
-        tr.append(td);
+
+var span = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+var chess = document.querySelector('.main');
+for (i = 0; i <= 8; i++) {
+    for (j = 0; j <= 8; j++) {
+        if ((i == 0 && j > 0) || (j == 0 && i > 0)) {
+            var item2 = document.createElement('div');
+            chess.appendChild(item2);
+            item2.classList.add('item2');
+            if (i == 0) item2.innerHTML = span[j - 1];
+            if (j == 0) item2.innerHTML = i;
+        }
+        else if (i > 0 && j > 0) {
+            var item = document.createElement('div');
+            chess.appendChild(item);
+            item.classList.add('item');
+        } else {
+            var item2 = document.createElement('div');
+            chess.appendChild(item2);
+            item2.classList.add('item2');
+        }
     }
 }
-
-document.querySelector('.main').append(table);
