ace868e61e87ef6c9a36af3ff63857f921108445
shomka <shaman_p@mail.ru>
2021-11-05 21:14:29 +0500

    1

--- test_js/1/project/js/main.js
+++ test_js/1/project/js/main.js
@@ -3,13 +3,15 @@ const products = [
     { id: 2, title: 'Mouse', price: 20, img: 'img/mouse.png' },
     { id: 3, title: 'Keyboard', price: 200, img: 'img/keyboard.png' },
     { id: 4, title: 'Gamepad', price: 50, img: 'img/gamepad.png' },
+    { id: 5, title: 'Headphones', price: 50, img: 'img/headphones.png' },
+    { id: 6, title: 'Сomputer chair', price: 50, img: 'img/computer_chair.png' },
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
 const renderProduct = (x) => {
     return `<div class="products-item">
-            <h3>${x.title}</h3>            
-            <img src=${x.img} alt="${x.title}">              
+                <img src=${x.img} alt="${x.title}">
+                <h3>${x.title}</h3>              
                 <p>Цена: ${x.price} руб.</p>
                 <button class="buy-btn">Купить</button>
             </div>`
