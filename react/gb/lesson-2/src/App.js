684aa4866a2b4eb9830cf5754df7d68f75b4cd94
shomka <shaman_p@mail.ru>
2022-03-17 22:40:50 +0500

    1

--- react/gb/lesson-2/src/App.js
+++ react/gb/lesson-2/src/App.js
@@ -1,8 +1,34 @@
-import './App.css';
+// import './App.css';
+
+import { useState } from 'react';
+import {useCreateTaskForm} from './hooks/useCreateTaskForm';
+import {useTaskList} from './hooks/useTaskList';
 
 function App() {
+
+const [] = useState(false)
+
+const {addNewTask, taskList, changeStatus} = useTaskList()
+
+const { handleSubmit, onChangeInput, inputValue} = useCreateTaskForm({onSubmit: addNewTask});
+
   return (
-    <div className="App">
+    <div>
+      <form onSubmit={handleSubmit}>
+          <input value={inputValue} onChange={onChangeInput} type="text" />
+          <button type='submit'>save</button>
+      </form>
+      <input type='checkbox'/>
+
+      <ul>
+        {
+          taskList.map(({status, text}, index) => {
+            return <li>
+              <input checked={status} type="checkbox" onChange={changeStatus(index, status)} /> {text}
+            </li>
+          })
+        }
+      </ul>
     </div>
   );
 }
--- react/gb/lesson-2/src/index.js
+++ react/gb/lesson-2/src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {Message} from './components/Message/message';
+//import {Message} from './components';
 
-const text = 'Привет'
-const text2 = 'Мир'
+// const text = 'Привет'
+// const text2 = 'Мир'
 
 ReactDOM.render(
   <React.StrictMode>
     <App/>
-    <Message textH1={text} textH2={text2}/>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+//    <Message textH1={text} textH2={text2}/>
