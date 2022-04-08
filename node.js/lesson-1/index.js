9a564d37c5c43683d8f2735e6fdf715a0d1e6f20
Shomka12 <shaman_p@mail.ru>
2022-04-08 12:27:21 +0500

    добавил проверку на числа

--- node.js/lesson-1/index.js
+++ node.js/lesson-1/index.js
@@ -1,6 +1,9 @@
-const args1 = +process.argv.slice(2,3);
-const args2 = +process.argv.slice(3);
+const args1 = Number(process.argv.slice(2,3));
+const args2 = Number(process.argv.slice(3));
+const color = ['green', 'yellow', 'red'];
+let colorItem = 0;
 
+if (!isNaN(args1) && !isNaN(args2)) {
 for (let j=args1;j<=args2;j++){
 let flag = true;
 for (let i = 2; i < j; i++) {
@@ -9,6 +12,12 @@ for (let i = 2; i < j; i++) {
 		break;
 	}
 }
-    if (flag) console.log(j,flag);
+    if (flag) {
+        console.log(j, color[colorItem]);
+        colorItem++;
+        if (colorItem>color.length-1) colorItem=0;
+    }
+}} else {
+console.log('Введите числовые значения'); 
+// break;
 }
-
