ddd861598ceea0ab29b1657c57dd509199927a86
shomka <shaman_p@mail.ru>
2022-08-25 16:01:09 +0500

    1

--- react/kamasutra/project1/src/components/Dialogs/Dialogs.jsx
+++ react/kamasutra/project1/src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css";
 
+const DialogItem = (props) => {
+  let path = "/dialogs/" + props.id;
+  return (
+    <div>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  );
+};
+
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div>
+      <div>
+        <DialogItem name="Dimych" id="1" />
+      </div>
+      <div>
+        <DialogItem name="Sveta" id="2" />
+      </div>
+      <div>
+        <DialogItem name="Petr" id="3" />
+      </div>
+      <div>
+        <DialogItem name="Nikolay" id="4" />
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
